16dca49816174b2906ef7095740c8555640e23d1
marinatic2 <73387238+marinatic2@users.noreply.github.com>
2021-02-17 09:45:15 +0100

    Add files via upload

---
+++ ejercicio 2 examen.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+//memory allocation
+
+int main(){
+	char *pletra;
+	int longitud;
+	printf("\ncuantas vocales: ");
+	scanf("%d", longitud);
+	pletra=(char *) malloc(longitud);
+	printf("\nescribe una palabra de menos de %d letras",longitud);
+	scanf("%s", pletra);
+	printf("\nAcabas de escribir la palabra%s", pletra);
+	
+	
+	return 0;
+}
+		
---
+++ pregunta 1 examen.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+#define ENTER *\n*
+using nanospace std;
+
+int main ()
+{
+	string palabra;
+	cout<<"Escribe una palabra";
+	getline(cin,palabra,ENTER);
+	
+	cout<<palabra;
+	atring alreves;
+	int tamaño=palbra.length();
+	for(int 1=tamaño=1/1>=0;1--)  {
+		cout<<palabra(1);
+		alreves=alreves+palabra(1);
+}
+cout<<alreves<<end1
+
+if (palabra==alreves) {
+	cout<<"ES PALÍNDROMO"<<END1/
+}else (
+	cout<<"NO ES PALÍNDROMO"<<end1;
+	)
+}
