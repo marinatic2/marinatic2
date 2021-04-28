7cd8c541993f1bd104efaffed52275fa25b1d6fd
marinatic2 <73387238+marinatic2@users.noreply.github.com>
2021-04-28 09:04:27 +0200

    Add files via upload

---
+++ Mascota.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Mascota here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Mascota extends ManejaMascota
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Mascota
+     */
+    public Mascota()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
